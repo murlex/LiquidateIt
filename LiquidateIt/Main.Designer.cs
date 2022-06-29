4930f629ddcd624c6b6551e00e670cdb42d9396f
Sunny <alexander.futoryan@sap.com>
2022-06-29 13:55:57 +0300

    Add project files.

---
+++ LiquidateIt/Main.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace LiquidateIt
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.templateTextBox = new System.Windows.Forms.TextBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // templateTextBox
+            // 
+            this.templateTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.templateTextBox.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.templateTextBox.Location = new System.Drawing.Point(12, 12);
+            this.templateTextBox.Multiline = true;
+            this.templateTextBox.Name = "templateTextBox";
+            this.templateTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.templateTextBox.Size = new System.Drawing.Size(1427, 234);
+            this.templateTextBox.TabIndex = 0;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTextBox.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.inputTextBox.Location = new System.Drawing.Point(12, 252);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.inputTextBox.Size = new System.Drawing.Size(1427, 234);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputTextBox.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.outputTextBox.Location = new System.Drawing.Point(12, 492);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputTextBox.Size = new System.Drawing.Size(1427, 270);
+            this.outputTextBox.TabIndex = 2;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(12, 765);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(56, 15);
+            this.errorLabel.TabIndex = 3;
+            this.errorLabel.Text = "No errors";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1451, 789);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.templateTextBox);
+            this.Name = "Main";
+            this.Text = "Liquidate it!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox templateTextBox;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label errorLabel;
+    }
+}
+
---
+++ LiquidateIt/Main.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Fluid;
+using Newtonsoft.Json.Linq;
+
+namespace LiquidateIt
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private readonly FluidParser _fluidParser = new();
+
+        private async Task<string> Render(object model)
+        {
+            var context = new TemplateContext(model);
+
+            var fluidTemplate = GetFluidTemplate(templateTextBox.Text);
+
+            return await fluidTemplate.RenderAsync(context);
+        }
+
+        // NOTE:
+        // For now the fluid template is not cached and reused since the serving endpoint which called to this method is cached,
+        // but if it will be needed - the Fluid template can be cached
+        private IFluidTemplate GetFluidTemplate(string htmlTemplate)
+        {
+            if (!_fluidParser.TryParse(htmlTemplate, out var fluidTemplate, out var error))
+            {
+                throw new InvalidOperationException("Error in the HTML template parsing");
+            }
+
+            return fluidTemplate;
+        }
+
+        private async void timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                outputTextBox.Text = await Render(JObject.Parse(inputTextBox.Text));
+            }
+            catch (Exception exception)
+            {
+                errorLabel.Text = exception.Message;
+            }
+        }
+    }
+}
